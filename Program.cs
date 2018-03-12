7da767c962d58e6eb9931797de3b0072fe8d0d22
Ericmei860315 <meieric0800@gmail.com>
2018-03-12 17:04:39 +0800

    作業

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\admin\Desktop\lab-2-zi-chuan-chu-li-Ericmei860315\Data\Input.txt");
+            string line = null;
+            while((line = sr.ReadLine()) !=null)
+            {
+                var lo = line.ToCharArray();
+                   
+                for (int i = 0; i < lo.Length; i++)
+                {
+                    if (lo[i].Equals('.') || lo[i].Equals('?') || lo[i].Equals('!'))
+                    {
+                        Console.WriteLine(lo[i]);
+                    }
+                    else {
+                        Console.Write(lo[i]);                    
+                    }
+                
+                }
+
+            } Console.Read();
+        }
+    }
+}
